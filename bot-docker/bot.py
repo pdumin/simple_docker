e38d7f529fe817485c8ea6ae062e4a551c60b633
pdumin <duminpn@gmail.com>
2021-12-23 12:06:02 +0300

    removing token and some fixes

--- bot-docker/bot.py
+++ bot-docker/bot.py
@@ -1,9 +1,10 @@
 import logging
 import requests
+from tok import tok
 
 from aiogram import Bot, Dispatcher, executor, types
 
-API_TOKEN = ''
+API_TOKEN = tok
 
 logging.basicConfig(level=logging.INFO)
 
@@ -14,7 +15,7 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands = ['api'])
 async def getapi(message: types.Message):
     ans = requests.get('http://172.17.0.2/home')
-    await message.answer(ans)
+    await message.answer(ans.text)
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
--- fast-api/main.py
+++ fast-api/main.py
@@ -3,8 +3,17 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get('/home_2')
+async def dictionary():
+    
+    return {
+        "source" : 'home_2',
+        "type" : 'another page'
+    }
+
 @app.get('/home')
 async def dictionary():
+
     return {
         "source" : 'home',
         "type" : 'another page'
