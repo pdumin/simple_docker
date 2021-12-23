f7377b0df8eefabcfb6057a5510ea81db0828d78
pdumin <duminpn@gmail.com>
2021-12-23 08:38:18 +0300

    bot update

--- bot-docker/bot.py
+++ bot-docker/bot.py
@@ -5,21 +5,15 @@ from aiogram import Bot, Dispatcher, executor, types
 
 API_TOKEN = ''
 
-# Configure logging
 logging.basicConfig(level=logging.INFO)
 
-# Initialize bot and dispatcher
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
-# @dp.message_handler()
-# async def echo(message: types.Message):
-#     await message.answer(message.text)
 
 @dp.message_handler(commands = ['api'])
 async def getapi(message: types.Message):
     ans = requests.get('http://172.17.0.2/home')
-    print(ans)
     await message.answer(ans)
 
 if __name__ == '__main__':
